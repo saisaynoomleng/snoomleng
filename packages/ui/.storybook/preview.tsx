391873f594b9e3683674c3257cebddf0272239c6
saisaynoomleng <saileng9723@gmail.com>
2026-08-12 16:35:31 -0400

    ui_design_system

--- packages/ui/.storybook/preview.tsx
+++ packages/ui/.storybook/preview.tsx
@@ -1,5 +1,6 @@
 import type { Preview } from '@storybook/react-vite';
 import { withThemeByClassName } from '@storybook/addon-themes';
+import '../src/globals.css';
 
 const preview: Preview = {
   parameters: {
---
+++ packages/ui/src/components/shared/Bounded.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const Bounded = (): React.JSX.Element => {
+  return <div></div>;
+};
