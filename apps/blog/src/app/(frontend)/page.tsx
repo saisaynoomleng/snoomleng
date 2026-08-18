454ef080c39647d14d031581d3a921f475bb5502
saisaynoomleng <saileng9723@gmail.com>
2026-08-18 13:40:37 -0400

    blog_home_page_pagination

--- apps/blog/src/app/(frontend)/page.tsx
+++ apps/blog/src/app/(frontend)/page.tsx
@@ -4,14 +4,20 @@ import { ALL_BLOGS_QUERY_ASC, ALL_BLOGS_QUERY_DESC } from '@/sanity/query';
 import { BlogCard, Bounded, Button } from '@snoomleng/ui';
 import clsx from 'clsx';
 import Link from 'next/link';
+import {
+  MdKeyboardDoubleArrowLeft,
+  MdKeyboardDoubleArrowRight,
+} from 'react-icons/md';
 import { twMerge } from 'tailwind-merge';
 
 export default async function Home({
   searchParams,
 }: {
-  searchParams: Promise<{ page: string; latest: boolean }>;
+  searchParams: Promise<{ page?: string; latest?: boolean }>;
 }) {
   const { page, latest } = await searchParams;
+
+  const isLatest = latest ? 'true' : 'false';
 
   const blogPerPage = 8;
   const currentPage = Number(page ?? '1');
@@ -45,6 +51,30 @@ export default async function Home({
 
       {totalPages > 1 && (
         <div className="col-span-full flex gap-x-2 items-center justify-center">
+          <Button variant="pagination" asChild disabled={currentPage === 1}>
+            <Link
+              href={{
+                pathname: '/',
+                query: {
+                  ...(latest && { latest }),
+                  page: currentPage > 1 ? currentPage - 1 : currentPage,
+                },
+              }}
+              className={twMerge(
+                clsx(
+                  'border-none',
+                  currentPage === 1 &&
+                    'pointer-events-none text-muted-foreground',
+                ),
+              )}
+            >
+              <span>
+                <MdKeyboardDoubleArrowLeft />
+              </span>
+              <span>Prev</span>
+            </Link>
+          </Button>
+
           {Array.from({ length: totalPages }, (_, i) => i + 1).map(
             (pageNum) => (
               <Button key={pageNum} asChild variant="pagination">
@@ -69,6 +99,35 @@ export default async function Home({
               </Button>
             ),
           )}
+
+          <Button
+            variant="pagination"
+            asChild
+            disabled={currentPage === totalPages}
+          >
+            <Link
+              href={{
+                pathname: '/',
+                query: {
+                  ...(latest && { latest }),
+                  page:
+                    currentPage === totalPages ? currentPage : currentPage + 1,
+                },
+              }}
+              className={twMerge(
+                clsx(
+                  'border-none',
+                  currentPage === totalPages &&
+                    'pointer-events-none text-muted-foreground',
+                ),
+              )}
+            >
+              <span>Next</span>
+              <span>
+                <MdKeyboardDoubleArrowRight />
+              </span>
+            </Link>
+          </Button>
         </div>
       )}
     </Bounded>
