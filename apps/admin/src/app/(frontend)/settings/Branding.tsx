f6adaaacbf8f16e2b84e6abb04bf0a1671901471
saisaynoomleng <saileng9723@gmail.com>
2026-08-21 11:34:15 -0400

    admin_settings

---
+++ apps/admin/src/app/(frontend)/settings/Branding.tsx
@@ -0,0 +1,9 @@
+import { Bounded } from '@snoomleng/ui';
+
+type BrandingProps = {
+  className?: string;
+};
+
+export const Branding = ({ className }: BrandingProps) => {
+  return <Bounded></Bounded>;
+};
--- apps/admin/src/sanity/query.ts
+++ apps/admin/src/sanity/query.ts
@@ -4,3 +4,45 @@ export const LOGO_QUERY = defineQuery(`*[_type == 'siteSetting'][0]{
   "imageUrl": primaryLogo.asset->url,
   "imageAlt": primaryLogo.alt
 }`);
+
+export const SETTING_QUERY = defineQuery(`*[_type == 'siteSetting'][0]{
+    "branding": {
+      siteName,
+      "primaryLogo": {
+        "imageUrl": primaryLogo.asset->url,
+        "imageAlt": primaryLogo.alt,
+      },
+      "secondaryLogo": {
+      "imageUrl": secondaryLogo.asset->url,
+      "imageAlt": secondaryLogo.alt,
+        },
+      socialLinks[]{
+        _key,
+        icon,
+        platform,
+        url
+      },
+      contactInfo,
+      mode[],
+      isAvailable,
+    },
+
+  "navigation": navigation[]{
+                  _key,
+                  href,
+                  isButton,
+                  label
+                },
+
+  "footer": {
+    "columns": footerColumns[]{
+      _key,
+      columnLinks[]{
+        _key,
+        href,
+        label
+      }
+    },
+    "text": footerText
+  }
+}`);
